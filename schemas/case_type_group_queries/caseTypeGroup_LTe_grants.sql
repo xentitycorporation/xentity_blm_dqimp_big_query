153a753fa1e9397bdc993260c3be9ee057cceda0
Mark Ferenc <122121606+Ferenczii@users.noreply.github.com>
2026-07-27 16:55:34 -0600

    Fix SQL case record type condition formatting

--- schemas/case_type_group_queries/caseTypeGroup_LTe_grants.sql
+++ schemas/case_type_group_queries/caseTypeGroup_LTe_grants.sql
@@ -11,7 +11,7 @@ EXECUTE IMMEDIATE FORMAT("""
             (
                 CASE_TYPE_CODE LIKE '26%%'
             ) 
-            AND (LOWER(CASE_RECORD_TYPES) NOT LIKE 'land transfer%%' OR CASE_RECORD_TYPES IS NULL)
+            AND (LOWER(CASE_RECORD_TYPES) NOT LIKE 'land_transfer%%' OR CASE_RECORD_TYPES IS NULL)
         )
     ORDER BY NAME
 """, snapshot_date);
